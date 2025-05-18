134084141bdd39712ec27a65b5689101ee90e473
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 18:57:42 +0000

    Declaração das variáveis

---
+++ 02AVENTUREIRO/aventureirodesafio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("DESAFIO LÓGICA SUPER TRUNFO - AVENTUREIRO! \n");
+
+    // Declaração das variáveis 
+
+    char estado1, estado2;
+    char codigoCidade1[5], codigoCidade2[5];
+    char nomeCidade1[50], nomeCidade2[50];
+    unsigned long int populacaoCidade1, populacaoCidade2;
+    float areaCidade1, areaCidade2;
+    float pibCidade1, pibCidade2;
+    int pontosTuristicosCidade1, pontosTuristicosCidade2;
+    float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
