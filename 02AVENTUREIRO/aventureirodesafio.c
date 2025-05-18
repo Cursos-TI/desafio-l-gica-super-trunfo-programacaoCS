ddde04e1b34e374f5001957e5f9c13bc4db63234
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 18:58:45 +0000

    Leitura da Carta 1

--- 02AVENTUREIRO/aventureirodesafio.c
+++ 02AVENTUREIRO/aventureirodesafio.c
@@ -13,3 +13,20 @@ int main() {
     float pibCidade1, pibCidade2;
     int pontosTuristicosCidade1, pontosTuristicosCidade2;
     float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
+
+    // Leitura da Carta 1
+
+    printf("Digite o estado da Carta 1 (A-H): ");
+    scanf(" %c", &estado1);
+    printf("Digite o código da Carta 1 (%c01-%c04): ", estado1, estado1);
+    scanf(" %s", codigoCidade1);
+    printf("Digite o nome da Cidade da Carta 1: ");
+    scanf(" %49s", nomeCidade1);
+    printf("Digite a população da Cidade da Carta 1: ");
+    scanf(" %lu", &populacaoCidade1);
+    printf("Digite a área da Cidade da Carta 1 (em km²): ");
+    scanf(" %f", &areaCidade1);
+    printf("Digite o PIB da Cidade da Carta 1 (em bilhões de reais): ");
+    scanf(" %f", &pibCidade1);
+    printf("Digite o número de pontos turísticos da Cidade da Carta 1: ");
+    scanf(" %d", &pontosTuristicosCidade1);
