ee6648ff491fc13f4214f9c091aeedb0a7618dc3
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 19:20:06 +0000

    Leitura da Carta 1

--- 03MESTRE/mestredesafio.c
+++ 03MESTRE/mestredesafio.c
@@ -14,3 +14,24 @@ int main() {
     float pibCidade1, pibCidade2;
     int pontosTuristicosCidade1, pontosTuristicosCidade2;
     float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
+
+     // Leitura dos dados da Carta 1
+
+     printf("Digite a primeira letra do país da Carta 1 (escolha de A-H): ");
+     scanf(" %c", &pais1);
+     printf("Digite o código da Carta 1 (%c01-%c04): ", pais1, pais1);
+     scanf(" %s", codigoCidade1);
+     printf("Digite o nome da Cidade da Carta 1: ");
+     scanf(" %49s", nomeCidade1);
+     printf("Digite a população da Cidade da Carta 1: ");
+     scanf(" %lu", &populacaoCidade1);
+     printf("Digite a área da Cidade da Carta 1 (em km²): ");
+     scanf(" %f", &areaCidade1);
+     printf("Digite o PIB da Cidade da Carta 1 (em bilhões de reais): ");
+     scanf(" %f", &pibCidade1);
+     printf("Digite o número de pontos turísticos da Cidade da Carta 1: ");
+     scanf(" %d", &pontosTuristicosCidade1);
+
+     
+
+
